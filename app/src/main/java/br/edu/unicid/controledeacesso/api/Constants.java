23587ea224adac059bc951e3a0053371ec092100
cdcon <cdcon@users.noreply.github.com>
2026-05-15 21:55:48 -0300

    config: troca BASE_URL para hotspot do notebook (192.168.137.1)
    Permite usar 2 celulares sem cabo durante apresentação.
    Notebooks com hotspot Windows sempre usam 192.168.137.1.
    Libera faixa 192.168.137.x no network_security_config.xml.

--- app/src/main/java/br/edu/unicid/controledeacesso/api/Constants.java
+++ app/src/main/java/br/edu/unicid/controledeacesso/api/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
     // public static final String BASE_URL = "http://10.0.2.2:8080/";
 
     // ▼ USE ESTE para rodar no CELULAR FÍSICO via USB (adb reverse tcp:8080 tcp:8080)
-    public static final String BASE_URL = "http://localhost:8080/";
+    // public static final String BASE_URL = "http://localhost:8080/";
 
-    // ▼ USE ESTE para rodar no CELULAR FÍSICO via Wi-Fi (mesma rede)
-    // public static final String BASE_URL = "http://172.26.110.77:8080/";
+    // ▼ USE ESTE para rodar no CELULAR FÍSICO via Hotspot do notebook (192.168.137.1 é fixo no Windows)
+    public static final String BASE_URL = "http://192.168.137.1:8080/";
 }
